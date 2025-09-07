a57374033d48fff660b6679fddd23a02a3fdc5ac
Sofia <sofiareis2018@gmail.com>
2025-09-07 16:06:27 -0700

    feat: not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h2>Oops... Page Not Found</h2>
+      <p>Could not find the requested page.</p>
+      <Link href="/" className="btn btn-primary my-5  inline-block">
+        Return Home
+      </Link>
+    </div>
+  );
+}
