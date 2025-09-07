b30460adee972faee01e45b9b3f5d5044bf7e443
Sofia <sofiareis2018@gmail.com>
2025-09-07 16:07:11 -0700

    feat: Projects and Resume content

---
+++ src/content/projects.ts
@@ -0,0 +1,90 @@
+import { CardProps } from "@/app/interfaces/interfaces";
+
+export const projects: CardProps[] = [
+  {
+    title: "Cards Tracker",
+    description: "A web app for managing your One Piece Card Game collection",
+    imageSrc: "/images/projects/onepiece_card.png",
+    page: "onepiece",
+  },
+  {
+    title: "Sharity",
+    description:
+      "(cmd-f Hackathon 2021) A mobile app that connects users to women's shelters",
+    imageSrc: "/images/projects/sharity_card.png",
+    page: "sharity",
+  },
+
+  {
+    title: "CareerCue",
+    description:
+      "(cmd-f Hackathon 2023) A web app that helps you practice for interviews by giving you customized feedback",
+    imageSrc: "/images/projects/careercue_card.png",
+    page: "careercue",
+  },
+
+  {
+    title: "PatientViz",
+    description:
+      "A desktop app to help physicians of the BC Children’s Hospital with the triage process of patients",
+    imageSrc: "/images/projects/patientviz_card.png",
+    page: "patientviz",
+  },
+
+  {
+    title: "Renew",
+    description:
+      "(cmd-f Hackathon 2022) A mobile app designed to provide support and resources for immigrants in a new country",
+    imageSrc: "/images/projects/renew_card.png",
+    page: "renew",
+  },
+
+  {
+    title: "Moko",
+    description:
+      "(Pinnacle Hackathon 2021) A mobile app that allows users to buy and sell fresh produce within their local communities",
+    imageSrc: "/images/projects/moko_card.png",
+    page: "moko",
+  },
+
+  {
+    title: "ChessMate",
+    description:
+      "A chess engine that features a hardware-accelerated chess algorithm and an Android app",
+    imageSrc: "/images/projects/chessmate_card.png",
+    page: "chessmate",
+  },
+
+  {
+    title: "Alacrity",
+    description:
+      "A web app to help students manage their time and accomplish tasks",
+    imageSrc: "/images/projects/alacrity_card.png",
+    page: "alacrity",
+  },
+  /*
+    {
+      title: 'Dancing Robot',
+      description: 'A robot that moves like it is dancing...',
+      imageSrc: dancingrobot,
+      page: 'dancingrobot',
+  
+    },
+
+    {
+      title: 'Robot Dog',
+      description: 'A robot that has dog life features (tail, voice control, movement)...',
+      imageSrc: robotdog,
+      page: 'robotdog',
+  
+    },
+
+    {
+      title: 'BobaBudy',
+      description: 'An app to determine which is your favourite Bubble Tea flavor',
+      imageSrc: boba,
+      page: 'bobabuddy',
+  
+    },
+    */
+];
