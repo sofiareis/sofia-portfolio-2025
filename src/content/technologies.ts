c91cefc7227e45e5504536f1a874fbb98f8f363c
Sofia <sofiareis2018@gmail.com>
2025-09-22 21:52:32 -0700

    feat: Add images of tech and prizes, tech content

---
+++ src/content/technologies.ts
@@ -0,0 +1,27 @@
+type techSquare = {
+  tech: string;
+  img: string;
+};
+
+export const technologies: techSquare[] = [
+  {
+    tech: "ReactJS",
+    img: "images/tech-logo/react.png",
+  },
+  {
+    tech: "NextJS",
+    img: "images/tech-logo/nextjs.svg",
+  },
+  {
+    tech: "NodeJS",
+    img: "images/tech-logo/nodejs.svg",
+  },
+  {
+    tech: "Tailwind",
+    img: "images/tech-logo/tailwind.svg",
+  },
+  {
+    tech: "ExpressJS",
+    img: "images/tech-logo/expressjs.svg",
+  },
+];
