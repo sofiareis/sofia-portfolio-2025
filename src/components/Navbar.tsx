48bb4725fa4c7573bf24dc7e01eb6109bad8d505
Sofia <sofiareis2018@gmail.com>
2025-09-24 20:33:51 -0700

    feat: Add max width for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ function Navbar() {
   /* Frame 2 */
 
   return (
-    <nav className="sticky md:top-10 top-6 z-50 max-h-[64px] min-h-[64px] bg-[#E2E2E2]/80 backdrop-blur-[2px] shadow-[0px_-1px_54px_-1px_rgba(0,0,0,0.25)] text-black md:mx-20 mx-5 rounded-4xl">
+    <nav className="sticky md:top-10 top-6 z-50 max-h-[64px] min-h-[64px] bg-[#E2E2E2]/80 backdrop-blur-[2px] shadow-[0px_-1px_54px_-1px_rgba(0,0,0,0.25)] text-black w-[90%] mx-auto max-w-[1500px] rounded-4xl">
       <div className="flex items-center h-full w-full">
         <div className="flex justify-evenly items-center w-full">
           {navbarLinks.map((link) => (
@@ -26,44 +26,25 @@ function Navbar() {
             </Link>
           ))}
         </div>
-        {/** 
-        <button
-          className="sm:hidden text-gray-800"
-          onClick={() => setMenuOpen(!menuOpen)}
-          aria-label="Toggle menu"
-        >
-          {menuOpen ? (
-            <FontAwesomeIcon
-              icon={faCircleXmark}
-              className="text-white cursor-pointer"
-            />
-          ) : (
-            <FontAwesomeIcon
-              icon={faBars}
-              className="text-white cursor-pointer"
-            />
-          )}
-        </button>
-        */}
       </div>
-      {/** 
-      {menuOpen && (
-        <div className="sm:hidden bg-[#223046]   px-6 py-4 space-y-4 shadow-lg">
-          {navbarLinks.map((link) => (
-            <Link
-              key={link.id}
-              href={link.url}
-              onClick={() => setMenuOpen(false)}
-              className="block  hover:text-[#C08791] transition-colors duration-200 font-medium"
-            >
-              {link.label}
-            </Link>
-          ))}
-        </div>
-      )}
-        */}
     </nav>
   );
 }
 
 export default Navbar;
+
+/*
+ <nav
+      className="fixed left-0 right-0 md:top-10 top-6 z-50 max-h-[64px] min-h-[64px] bg-[#E2E2E2]/80 backdrop-blur-[2px] shadow-[0px_-1px_54px_-1px_rgba(0,0,0,0.25)] text-black mx-auto w-[90%] max-w-[1500px] rounded-4xl px-6 
+             flex items-center justify-evenly "
+    >
+      {navbarLinks.map((link) => (
+        <Link
+          key={link.id}
+          href={link.url}
+          className={`${pathname === `${link.url}` ? "text-white bg-primary " : "hover:text-primary"} md:text-base text-sm transition-all duration-200 font-medium px-3 py-2 rounded-4xl`}
+        >
+          {link.label}
+        </Link>
+      ))}
+        */
