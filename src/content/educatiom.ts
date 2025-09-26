10e4f20951c1bb3c8ffc893e31f0cfa1197baed4
Sofia <sofiareis2018@gmail.com>
2025-09-25 21:28:13 -0700

    feat: Education content

---
+++ src/content/educatiom.ts
@@ -0,0 +1,15 @@
+export const education = [
+  {
+    title: "Bachelor of Applied Science, Computer Engineering",
+    subtitle: "University of British Columbia",
+    description: "Graduated with Distinction",
+    logo: "images/education/ubc-logo2.png",
+  },
+  {
+    title: "Machine Learning Specialization",
+    subtitle: "DeepLearning.AI, Stanford University",
+    description:
+      "Completed all three courses of the Machine Learning Specialization - Supervised Machine Learning: Regression and Classification; Advanced Learning Algorithms; Unsupervised Learning, Recommenders, Reinforcement Learning",
+    logo: "images/education/deeplearning-logo.svg",
+  },
+];
